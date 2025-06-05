2262db1fdc7600aef471a585d00a02225a7ef98b
arnim2006 <24ucc030@lnmiit.ac.in>
2025-06-05 01:01:01 +0000

    react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+ 
+
+  return (
+    <h1>chai aur react with vite | Arnim: today i have started my react journey and i have learned to make a react app and projects </h1>
+  )
+}
+
+export default App

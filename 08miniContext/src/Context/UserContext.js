5dca1f9328e58093aac853c7ae0a04959ec356d2
arnim2006 <24ucc030@lnmiit.ac.in>
2025-06-17 22:30:02 +0000

    .

--- 08miniContext/src/Context/UserContext.js
+++ 08miniContext/src/Context/UserContext.js
@@ -1,8 +1,6 @@
 // step 1
-
 import React from 'react'
 
 const UserContext = React.createContext()
 
-export default UserContext;  // it will act as a provider , iske andar jo bhi components honge usko "UserContext = React.createContext()" ka access mil  jayega
-
+export default UserContext;
